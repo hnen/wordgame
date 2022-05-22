a01a07227cd50f8ebe21df066a9fbd24ab1353f6
Harri Hätinen <harri.hatinen@supercell.com>
2022-05-22 18:26:29 +0300

    quick and dirty schema migration by version check

--- schema.sql
+++ schema.sql
@@ -1,5 +1,26 @@
+DO
+$do$
+
+DECLARE schema_version INT := 1; /* INCREMENT THIS VALUE TO RECREATE TABLES */
+
+BEGIN
+
+IF NOT EXISTS (SELECT version FROM schema WHERE version >= schema_version) THEN
+    /* Reset tables */
+    DROP SCHEMA public CASCADE;
+    CREATE SCHEMA public;
+    GRANT ALL ON SCHEMA public TO postgres;
+    GRANT ALL ON SCHEMA public TO public;
+
+    /* Record Schema Version */
+    CREATE TABLE IF NOT EXISTS schema ( version INT );
+    INSERT INTO schema(version) VALUES ( schema_version );
+
+    /* SCHEMA GOES HERE */
 
 
+END IF;
 
-CREATE TABLE IF NOT EXISTS schema ( version INT );
-INSERT INTO schema(version) VALUES (1);
+END
+$do$
+
