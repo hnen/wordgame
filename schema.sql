a82265b40b54ade296ee05b86a68aea91145fd9e
Harri Hätinen <harri.hatinen@supercell.com>
2022-05-24 20:40:02 +0300

    Add word, theme and word_theme to schema. Add some placeholder rows for testing.

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 DO
 $do$
 
-DECLARE schema_version INT := 1; /* INCREMENT THIS VALUE TO RECREATE TABLES */
+DECLARE schema_version INT := 2; /* INCREMENT THIS VALUE TO RECREATE TABLES */
 
 BEGIN
 
@@ -17,7 +17,55 @@ IF NOT EXISTS (SELECT version FROM schema WHERE version >= schema_version) THEN
     INSERT INTO schema(version) VALUES ( schema_version );
 
     /* SCHEMA GOES HERE */
+    CREATE TABLE word (
+        id serial PRIMARY KEY,
+        word VARCHAR( 128 ) UNIQUE
+    );
 
+    CREATE TABLE theme (
+        id serial PRIMARY KEY,
+        theme_name VARCHAR( 128 )
+    );
+
+    CREATE TABLE word_theme (
+        id serial PRIMARY KEY,
+        word_id INT,
+        theme_id INT,
+        FOREIGN KEY (word_id) REFERENCES word(id),
+        FOREIGN KEY (theme_id) REFERENCES theme(id)
+    );
+
+    INSERT INTO word VALUES ( DEFAULT, 'avain' );
+    INSERT INTO word VALUES ( DEFAULT, 'apina' );
+    INSERT INTO word VALUES ( DEFAULT, 'kirja' );
+    INSERT INTO word VALUES ( DEFAULT, 'aurinko' );
+    INSERT INTO word VALUES ( DEFAULT, 'zorro' );
+    INSERT INTO word VALUES ( DEFAULT, 'tarzan' );
+
+    INSERT INTO theme VALUES ( DEFAULT, 'Helpot Sanat' );
+    INSERT INTO theme VALUES ( DEFAULT, 'Kovikset' );
+
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='avain' ), 
+        ( SELECT id FROM theme WHERE theme_name='Helpot Sanat' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='apina' ), 
+        ( SELECT id FROM theme WHERE theme_name='Helpot Sanat' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='kirja' ), 
+        ( SELECT id FROM theme WHERE theme_name='Helpot Sanat' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='apina' ), 
+        ( SELECT id FROM theme WHERE theme_name='Kovikset' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='aurinko' ), 
+        ( SELECT id FROM theme WHERE theme_name='Helpot Sanat' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='zorro' ), 
+        ( SELECT id FROM theme WHERE theme_name='Kovikset' )  );
+    INSERT INTO word_theme VALUES ( DEFAULT, 
+        ( SELECT id FROM word WHERE word='tarzan' ), 
+        ( SELECT id FROM theme WHERE theme_name='Kovikset' )  );
 
 END IF;
 
