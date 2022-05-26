aa8a38d063552f3094ce5d01bdf9cabcc3b874b0
Harri Hätinen <harri.hatinen@supercell.com>
2022-05-26 09:36:22 +0300

    Refactor functionality into classes: UserDao for db managing, GameSession for session management, Word is a data class for word

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, session
 from flask import render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
 
 app = Flask(__name__, static_url_path='/static')
-app.secret_key = '715517'
+app.secret_key = getenv("SECRET_KEY")
 app.debug = True
 
 uri = getenv("DATABASE_URL")
@@ -22,11 +22,52 @@ def index():
 def game():
     return render_template( "game.html" )
 
+class GameSession:
+    KEY_WORD_ID = "game_word_id"
+
+    def is_active():
+        return (self.KEY_WORD_ID in session) and session[self.KEY_WORD_ID] > 0
+
+    def get_word_id(self):
+        if self.is_active():
+            return session[self.KEY_WORD_ID]
+        return -1
+
+    def set_word_id(self, id : int):
+        session[self.KEY_WORD_ID] = id
+
+    def start(self, word_id):
+        self.set_word_id(word_id)
+
+    def expire(self):
+        session.pop(self.KEY_WORD_ID, None)
+
+class Word:
+    id = -1
+    word = ""
+
+    def __init__(self, word_id : int, word : str):
+        self.word_id = word_id
+        self.word = word
+
+class UserDao:
+    def _unpack_word(self, result) -> Word:
+        tp = result.fetchone()._mapping
+        return Word( tp["id"], tp["word"] )
+
+    def select_random_word(self) -> Word:
+        result = db.session.execute("SELECT * FROM word LIMIT 1")
+        return self._unpack_word(result)
+
 @app.route('/action_game_start', methods=['POST'])
 def game_start():
-    result = db.session.execute("SELECT word FROM word LIMIT 1")
-    word = result.fetchone()._mapping["word"]
-    response = { 'word_length': len(word), 'word': word }
+    game_session = GameSession()
+    dao = UserDao()
+
+    word = dao.select_random_word()
+    game_session.start(word.id)
+    
+    response = { 'word_length': len(word.word), 'word': word.word }
     return jsonify( response )
 
 
