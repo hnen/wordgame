30643522efd339684a0b9ad20fc9b845d01e0e77
Harri Hätinen <harri.hatinen@supercell.com>
2022-05-28 14:53:30 +0300

    refactoring game.js

--- wordgame/static/js/game.js
+++ wordgame/static/js/game.js
@@ -3,22 +3,91 @@ class LetterInputs
 {
     constructor()
     {
-        this._elements = []
+        this._lines = []
     }
 
-    getInputElements(line_number)
+    _getInputElements(line_number)
     {
-        return this._elements[line_number];
+        return this._lines[line_number];
     }
 
-    getLetter(line_number, letter_index)
+    _getLetter(line_number, letter_index)
     {
-        return this.getInputElements(line_number)[letter_index];
+        return this._getInputElements(line_number)[letter_index];
     }
 
-    setLetterResult(line_number, letter_index, result)
+    _pushNewLine()
     {
-        let letter = this.getLetter(line_number, letter_index);
+        let new_line = [];
+        this._lines.push(new_line);
+        return new_line;
+    }
+
+    _getId(line, letter)
+    {
+        let id = "input_letter_" + line + "_" + letter;
+        return id;
+    }
+
+    _funcLetterInput(next_id, is_last_letter)
+    {
+        return function()
+        { 
+            let next = $("#" + next_id);
+            if ( !($(this).val() === "") )
+            {
+                if ( !is_last_letter )
+                    next.focus();
+                else
+                    postGuess();
+            }
+        };
+    }
+
+    _funcLetterKeyDown(prev_id)
+    {
+        return function(e) 
+        {
+            let prev = $("#" + prev_id);
+            if ((e.which == 8 || e.which == 46) && $(this).val() =='') {
+                prev.focus();
+            }
+        };        
+    }
+
+    _createLetter(is_last_letter)
+    {
+        let line = this._getCurrLineIndex();
+        let char = this._getCurrLine().length;
+        let id = this._getId(line, char);
+        $("#game_area").append( '<input class="letter" type="text" autocomplete="off" id="' + id + '" maxlength="1">' );
+        let el = $("#" + id);
+
+        let next_id = this._getId(line, char + 1)
+        let prev_id  = this._getId(line, char - 1)
+
+        el            
+            .on( 'input', this._funcLetterInput( next_id, is_last_letter ) ) // On entering letter, move to next field (unless emptied)            
+            .keydown( this._funcLetterKeyDown( prev_id ) ); // On backspace, move to previous
+
+        this._lines[line].push(el);
+
+        return el;
+    }
+
+    _getCurrLineIndex()
+    {
+        return this._lines.length - 1;
+    }
+
+    _getCurrLine()
+    {
+        return this._lines[this._getCurrLineIndex()]
+    }
+
+    setLetterResult(letter_index, result)
+    {
+        let letter = this._getLetter(this._getCurrLineIndex(), letter_index);
         if ( result === "WRONG" )
         {
             letter.addClass( "letter_wrong" )
@@ -30,87 +99,38 @@ class LetterInputs
         else if ( result === "CORRECT" )
         {
             letter.addClass( "letter_correct" )
-        }          
-    }
-
-    pushNewLine()
-    {
-        let new_line = [];
-        this._elements.push(new_line);
-        return new_line;
-    }
-
-    getId(line, letter)
-    {
-        let id = "input_letter_" + line + "_" + letter;
-        return id;
-    }
-
-    pushInputElement(is_last_letter)
-    {
-        let line = this.getCurrLine();
-        let char = this._elements[this.getCurrLine()].length;
-        let id = this.getId(line, char);
-        $("#game_area").append( '<input class="letter" type="text" autocomplete="off" id="' + id + '" maxlength="1">' );
-        let el = $("#" + id);
-
-        let next_id = this.getId(line, char + 1)
-        let prev_id  = this.getId(line, char - 1)
-
-        el
-            // On entering letter, move to next field (unless emptied)
-            .on( 'input', 
-                function(next_id, is_last_letter) { 
-                    return function()
-                    { 
-                        let next = $("#" + next_id);
-                        if ( !($(this).val() === "") )
-                        {
-                            if ( !is_last_letter )
-                                next.focus();
-                            else
-                                postGuess();
-                        }
-                    } 
-                } ( next_id, is_last_letter ) 
-            )
-            // On backspace, move to previous
-            .keydown(
-                function(prev_id) {
-                    return function(e) 
-                    {
-                        let prev = $("#" + prev_id);
-                        if ((e.which == 8 || e.which == 46) && $(this).val() =='') {
-                            prev.focus();
-                        }
-                    } 
-                } (prev_id)
-            );
-
-        this._elements[line].push(el);
-
-        return el;
-    }
-
-    getCurrLine()
-    {
-        return this._elements.length - 1;
-    }
+        }
+    }    
 
     createNewLine(length)
     {
-        let first_id = this.getId( this.getCurrLine(), 0 );    
-        game_state.letter_inputs.pushNewLine();
+        let first_id = this._getId( this._getCurrLineIndex(), 0 );    
+        this._pushNewLine();
     
         for ( var i = 0; i < length; i++ )
         {
             let is_last_letter = i == length - 1;
-            game_state.letter_inputs.pushInputElement(is_last_letter);
+            this._createLetter(is_last_letter);
         }
         
         $("#game_area").append("<br>");
     
-        this._elements[this.getCurrLine()][0].focus();        
+        this._getCurrLine()[0].focus();        
+    }
+
+    
+    getGuess()
+    {
+        let curr_letters = this._getCurrLine()
+
+        let guess = "";
+        for ( const letter of curr_letters )
+        {
+            letter.prop( "disabled", true );
+            guess += letter.val();
+        }
+
+        return guess;
     }
 
 }
@@ -127,14 +147,14 @@ class Game
 
     getCurrentInputs()
     {
-        return this.letter_inputs.getInputElements( this.line_number );
+        return this.letter_inputs._getInputElements( this.line_number );
     }
 
     colorizeInputsWithResults(result)
     {
         for ( var i = 0; i < result.length; i++ )
         {
-            this.letter_inputs.setLetterResult( this.line_number, i, result[i] );
+            this.letter_inputs.setLetterResult( i, result[i] );
         }        
     }
 
@@ -154,9 +174,8 @@ function postStart()
 
 function postGuess()
 {
-    $.post( "/game/guess", { 'guess': getGuess() } )
+    $.post( "/game/guess", { 'guess': game_state.letter_inputs.getGuess() } )
         .done( function( data ) {
-            curr_letters = game_state.getCurrentInputs();
             game_state.colorizeInputsWithResults(data.result)
             game_state.line_number++;
             $( "#text_game_status" ).text( JSON.stringify(data) );
@@ -176,18 +195,6 @@ function startNewWord(word_length)
     game_state.letter_inputs.createNewLine( word_length );
 }
 
-function getGuess()
-{
-    curr_letters = game_state.letter_inputs._elements[game_state.line_number];
-    guess = "";
-    for ( const letter of curr_letters )
-    {
-        letter.prop( "disabled", true );
-        guess += letter.val();
-    }
-
-    return guess;
-}
 
 
 
